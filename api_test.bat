47a8fb7c9a218b30fb775985d29fca641d608a24
Kavita Jadhav <kavita.jadhav1109@gmail.com>
2025-02-21 20:53:09 -0600

    final project

---
+++ api_test.bat
@@ -0,0 +1,1 @@
+curl -X POST "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict" -H "Content-Type: application/json" -d '{"text": "I am happy today."}'
